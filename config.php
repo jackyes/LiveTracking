df4453033d98cfdb3b3cd6d40760fe2661bbaaa2
jackyes <jackyes@users.noreply.github.com>
2021-09-26 11:13:14 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
 $SecretKey = '12345';  			// Secret key to pass as key parameter in http get
 $ShowOnlyLastPos = true;  		// Show only the last available position
+$MaxPoint = 1000;               // Maxium number of point displayed on map
 ?>
